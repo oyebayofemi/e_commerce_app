9aff645028c1aed2a845ae2b8c9b83b575d1da6c
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-05-18 17:11:27 +0100

    cart provider

--- lib/screens/cartpage.dart
+++ lib/screens/cartpage.dart
@@ -1,21 +1,22 @@
+import 'package:e_commerce_app/providers/cart_provider.dart';
 import 'package:e_commerce_app/screens/checkout_screen.dart';
+import 'package:e_commerce_app/shared/widget.dart';
+import 'package:e_commerce_app/shared/widget/cartlistproduct_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
-  String url;
-  String name;
-  int amount;
-  CartPage({required this.url, required this.amount, required this.name});
-
   @override
   State<CartPage> createState() => _CartPageState();
 }
 
 class _CartPageState extends State<CartPage> {
-  int quantity = 0;
+  // int quantity = 0;
   @override
   Widget build(BuildContext context) {
+    CartProvider cartProvider = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart'),
@@ -48,114 +49,22 @@ class _CartPageState extends State<CartPage> {
               ),
             )),
       ),
-      body: Container(
-          // color: Colors.blue,
-          child: ListView(
-        children: [
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-          cartListProduct(context, widget.url, widget.name, widget.amount),
-        ],
-      )),
-    );
-  }
-
-  Container cartListProduct(
-      BuildContext context, String url, String name, int amount) {
-    return Container(
-      height: MediaQuery.of(context).size.height - 1910.h,
-      child: Card(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height - 1950.h,
-                  width: MediaQuery.of(context).size.width - 620.w,
-                  // color: Colors.red,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.contain,
-                        image: NetworkImage(
-                          url,
-                        )),
-                  ),
-                  // child: Text('s'),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height - 1950.h,
-                  width: MediaQuery.of(context).size.width - 620.w,
-                  child: ListTile(
-                    title: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('$name'),
-                        Text('CLoths'),
-                        Text(
-                          '\$ $amount',
-                          style: TextStyle(color: Colors.grey[500]),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width - 600.w,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            children: [
-                              ButtonTheme(
-                                minWidth:
-                                    MediaQuery.of(context).size.width - 990.w,
-                                child: FlatButton(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(50))),
-                                    color: Colors.grey.shade200,
-                                    onPressed: () {
-                                      setState(() {
-                                        quantity--;
-                                        if (quantity < 0) {
-                                          quantity = 0;
-                                        }
-                                      });
-                                    },
-                                    child: Text('-')),
-                              ),
-                              Text('$quantity'),
-                              ButtonTheme(
-                                minWidth:
-                                    MediaQuery.of(context).size.width - 990.w,
-                                child: FlatButton(
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(50))),
-                                    color: Colors.grey.shade200,
-                                    onPressed: () {
-                                      setState(() {
-                                        quantity++;
-                                      });
-                                    },
-                                    child: Text('+')),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+      body: cartProvider.getCartModelList.isEmpty
+          ? load()
+          : Container(
+              // color: Colors.blue,
+              child: ListView.builder(
+              itemCount: cartProvider.getCartModelListLength,
+              itemBuilder: (context, index) {
+                return CartListProductWidget(
+                  amount: cartProvider.getCartModelList[index].amount,
+                  name: cartProvider.getCartModelList[index].name,
+                  quantity: cartProvider.getCartModelList[index].quantity,
+                  url: cartProvider.getCartModelList[index].url,
+                  isCheckOutPage: false,
+                );
+              },
+            )),
     );
   }
 }
